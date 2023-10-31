1f65255d89e83a6b0b7050cb8da02265f1a742bc
Caitlin Ngo <ngoca@vcu.edu>
2023-10-31 15:54:03 -0400

    My First Unit Test

---
+++ test-simple-math.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "simple-math.c"
+
+int main() {
+	int result = addingTen(5);
+	if (result == 15){
+		printf("Test passed\n");
+		return 0;
+	}
+	else {
+		printf("Test failed\n");
+		return 1;
+	}
+}
